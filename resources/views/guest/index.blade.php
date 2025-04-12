7c13ada054e3d80c2132252fc9b64575dcaf421f
Tora0815 <codetora6@gmail.com>
2025-04-13 00:19:53 +0900

    index.blade.phpをBootstrapデザインに修正

--- resources/views/guest/index.blade.php
+++ resources/views/guest/index.blade.php
@@ -1,6 +1,7 @@
-<x-guest-layout>
+<x-base-layout>
     <x-slot name="title">ブログ一覧</x-slot>
     <x-slot name="slot">
+
         <div class="container mt-5">
             <h1>ブログ一覧</h1>
             <p><a href="/new" class="btn btn-primary">新規追加</a></p>
@@ -9,17 +10,27 @@
                 <div class="card mb-2">
                     <div class="card-body">
                         <h4 class="card-title">{{ $article->title }}</h4>
-                        <p class="card-subtitle text-muted">{{ $article->updated_at }}</p>
+                        <p class="card-subtitle mb-2 text-muted">{{ $article->updated_at }}</p>
                         <p class="card-text">{{ $article->body }}</p>
 
+                        <!-- 編集ボタン -->
                         <form method="post" action="/edit" style="display:inline;">
                             @csrf
                             <input type="hidden" name="edit_id" value="{{ $article->id }}">
                             <button class="btn btn-sm btn-outline-secondary" type="submit">編集</button>
                         </form>
+
+                        <!-- 削除ボタン -->
+                        <form method="post" action="/delete" style="display:inline;">
+                            @csrf
+                            <input type="hidden" name="delete_id" value="{{ $article->id }}">
+                            <button class="btn btn-sm btn-outline-secondary" type="submit">削除</button>
+                        </form>
                     </div>
                 </div>
             @endforeach
+
         </div>
+
     </x-slot>
-</x-guest-layout>
+</x-base-layout>
